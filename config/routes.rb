9f95127c54b23434074314f9e5043278875cf225
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-12-21 21:27:31 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/home" , to: "persons#home"
   post "/login", to: "persons#login"
+  get "/staff/home/:id",to: "staff#home"
+  get "/students/home/:id",to: "students#home"
 end
