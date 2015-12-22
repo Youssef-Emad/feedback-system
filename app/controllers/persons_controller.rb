6470b91bc6ccd02bc2347aae61e748c685a21572
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-12-22 09:23:44 +0200

    add logout

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,6 +1,6 @@
  class PersonsController < ApplicationController
  	skip_before_filter :verify_authenticity_token
- 	before_filter :check_logged_in
+ 	before_filter :check_logged_in , only: [:home]
 	def home
 	end
 
@@ -8,6 +8,12 @@
 		email = params["email"]
 		password =  params["password"]
 		check_existance(email,password)
+	end
+
+	def logout
+		session[:id] = nil
+		session[:is_staff] = nil
+		redirect_to "/home"
 	end
 end
 
@@ -17,9 +23,9 @@ def check_existance(email,password)
 		session[:id] = result.first[0]
 		session[:is_staff] = result.first[1]
 		if result.first[1] == 1
-			redirect_to "/staff/home/#{result.first[0]}"
+			redirect_to "/staff/#{result.first[0]}"
 		else result.first[1] == 1
-			redirect_to "/students/home/#{result.first[0]}"
+			redirect_to "/students/#{result.first[0]}"
 		end
 	else
 		render 'home'
@@ -29,9 +35,9 @@ end
 def check_logged_in
 	if session[:id] != nil
 		if session[:is_staff] == 1
-			redirect_to "/staff/home/#{session[:id]}"
+			redirect_to "/staff/#{session[:id]}"
 		else session[:is_staff] == 0
-			redirect_to "/students/home/#{session[:id]}"
+			redirect_to "/students/#{session[:id]}"
 		end
 	end
 end
